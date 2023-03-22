35689b7e8c3c27930470b6b8306b324c7070643b
Koeninger <qkoenin@okstate.edu>
2023-03-22 16:59:58 -0500

    Finished SV and ModeSim testing, ready for implementation

--- Lab3/SV/fsm.sv
+++ Lab3/SV/fsm.sv
@@ -1,12 +1,22 @@
-module FSM (clk, reset, a, y);
+module FSM (clk, reset, Lin, Rin, H, Li, Lm, Lo, Ri, Rm, Ro);
 
    input logic  clk;
    input logic  reset;
-   input logic 	a;
+   input logic 	Lin;
+   input logic  Rin;
+   input logic  H;
    
-   output logic y;
+   output logic Li;
+   output logic Lm;
+   output logic Lo;
+   output logic Ri;
+   output logic Rm;
+   output logic Ro;
 
-   typedef enum 	logic [1:0] {S0, S1, S2} statetype;
+   logic L;
+   logic R;
+
+   typedef enum 	logic [1:0] {S0, S1, S2, S3} statetype;
    statetype state, nextstate;
    
    // state register
@@ -16,25 +26,121 @@ module FSM (clk, reset, a, y);
    
    // next state logic
    always_comb
+     case (H)
+      1'b1: begin
+        L <= 1'b1;
+        R <= 1'b1;
+      end
+      default: begin
+        L <= Lin;
+        R <= Rin;
+      end
+     endcase
+   always_comb
      case (state)
        S0: begin
-	  y <= 1'b0;	  
-	  if (a) nextstate <= S0;
-	  else   nextstate <= S1;
+	      // y <= 1'b0;	  
+	      // if (a) nextstate <= S0;
+	      // else   nextstate <= S1;
+
+        Li <= 1'b0;
+        Lm <= 1'b0;
+        Lo <= 1'b0;
+        Ri <= 1'b0;
+        Rm <= 1'b0;
+        Ro <= 1'b0;
+
+        if (L || R) nextstate <= S1;
+        else nextstate <= S0;
        end
        S1: begin
-	  y <= 1'b0;	  	  
-	  if (a) nextstate <= S2;
-	  else   nextstate <= S1;
+	      // y <= 1'b0;	  	  
+	      // if (a) nextstate <= S2;
+	      // else   nextstate <= S1;
+
+        if (L) begin
+          Li <= 1'b1;
+          Lm <= 1'b0;
+          Lo <= 1'b0;
+        end else begin
+          Li <= 1'b0;
+          Lm <= 1'b0;
+          Lo <= 1'b0;
+        end
+        if (R) begin
+          Ri <= 1'b1;
+          Rm <= 1'b0;
+          Ro <= 1'b0;
+        end else begin
+          Ri <= 1'b0;
+          Rm <= 1'b0;
+          Ro <= 1'b0;
+        end
+
+        if (L || R) nextstate <= S2;
+        else nextstate <= S0;
        end
        S2: begin
-	  y <= 1'b1;	  	  
-	  if (a) nextstate <= S2;
-	  else   nextstate <= S0;
+	      // y <= 1'b1;	  	  
+	      // if (a) nextstate <= S2;
+	      // else   nextstate <= S0;
+
+        if (L) begin
+          Li <= 1'b1;
+          Lm <= 1'b1;
+          Lo <= 1'b0;
+        end else begin
+          Li <= 1'b0;
+          Lm <= 1'b0;
+          Lo <= 1'b0;
+        end
+        if (R) begin
+          Ri <= 1'b1;
+          Rm <= 1'b1;
+          Ro <= 1'b0;
+        end else begin
+          Ri <= 1'b0;
+          Rm <= 1'b0;
+          Ro <= 1'b0;
+        end
+
+        if (L || R) nextstate <= S3;
+        else nextstate <= S0;
+       end
+       S3: begin
+	      // y <= 1'b1;	  	  
+	      // if (a) nextstate <= S2;
+	      // else   nextstate <= S0;
+
+        if (L) begin
+          Li <= 1'b1;
+          Lm <= 1'b1;
+          Lo <= 1'b1;
+        end else begin
+          Li <= 1'b0;
+          Lm <= 1'b0;
+          Lo <= 1'b0;
+        end
+        if (R) begin
+          Ri <= 1'b1;
+          Rm <= 1'b1;
+          Ro <= 1'b1;
+        end else begin
+          Ri <= 1'b0;
+          Rm <= 1'b0;
+          Ro <= 1'b0;
+        end
+
+        nextstate <= S0;
        end
        default: begin
-	  y <= 1'b0;	  	  
-	  nextstate <= S0;
+	      Li <= 1'b0;
+        Lm <= 1'b0;
+        Lo <= 1'b0;
+        Ri <= 1'b0;
+        Rm <= 1'b0;
+        Ro <= 1'b0;	  	  
+	      nextstate <= S0;
        end
      endcase
    
--- Lab3/SV/fsm_tb.sv
+++ Lab3/SV/fsm_tb.sv
@@ -2,16 +2,16 @@
 module stimulus ();
 
    logic  clk;
-   logic  a;
+   logic  [7:0] sw;
    logic  reset;
    
-   logic  y;
+   logic  [7:0] led;
    
    integer handle3;
    integer desc3;
    
    // Instantiate DUT
-   FSM dut (clk, reset, a, y);   
+   FSM dut (clk, reset, sw[7], sw[0], sw[3], led[5], led[6], led[7], led[2], led[1], led[0]);   
    
    // Setup the clock to toggle every 1 time units 
    initial 
@@ -25,23 +25,30 @@ module stimulus ();
 	// Gives output file name
 	handle3 = $fopen("fsm.out");
 	// Tells when to finish simulation
-	#500 $finish;		
+	#600 $finish;		
      end
 
    always 
      begin
 	desc3 = handle3;
-	#5 $fdisplay(desc3, "%b %b || %b", 
-		     reset, a, y);
+	#10 $fdisplay(desc3, "%b %b %b %b || %b %b %b 0 0 %b %b %b", 
+		     reset, sw[7], sw[0], sw[3], led[7], led[6], led[5], led[2], led[1], led[0]);
      end   
    
    initial 
      begin      
 	#0   reset = 1'b1;
 	#41  reset = 1'b0;	
-	#0   a = 1'b0;
-	#20  a = 1'b1;
-	#20  a = 1'b0;
+	#0   sw[7:0] = 8'b10000000;
+	#80  sw[7:0] = 8'b00000000;
+	#20  sw[7:0] = 8'b00000001;
+     #80  sw[7:0] = 8'b00000000;
+     #20  sw[7:0] = 8'b10000001;
+     #80  reset = 1'b1;
+     #40  reset = 1'b0;
+     #0  sw[7:0] = 8'b00000001;
+     #40 sw[7:0] = 8'b00001001;
+     #80 sw[7:0] = 8'b10000000;
      end
 
 endmodule // FSM_tb
